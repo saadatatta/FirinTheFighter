b9fa695ebba2c1d0c5ccc409b1b6e72a0918a867
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2018-07-09 15:18:26 +0500

    Cinemachine camera added.
    Cinemachine camera displays the player.

---
+++ Assets/Scripts/DeactivateObjectsOnGameStart.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class DeactivateObjectsOnGameStart : MonoBehaviour {
+
+    [SerializeField] private GameObject[] gameObjects;
+	
+    // Use this for initialization
+	void Start () {
+        foreach (GameObject item in gameObjects)
+        {
+            item.SetActive(false);
+        }
+	}
+	
+	
+}
---
+++ Assets/Scripts/VirtualCameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class VirtualCameraFollow : MonoBehaviour {
+
+    private CinemachineVirtualCamera virtualCamera;
+
+	// Use this for initialization
+	void Start () {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        virtualCamera.Follow = GameManager.Instance.Player;
+	}
+	
+	
+}
